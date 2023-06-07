7bd5d2291fdf23b265e9e4aa43819261e55a043d
Sadik Raida <sadik.raida@gmail.com>
2023-06-07 10:43:37 +0200

    oublie d'un ;

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { AuthenticationModule } from './authentication/authentication.module'
+import { AuthenticationModule } from './authentication/authentication.module';
 import { PassModule } from './pass/pass.module';
 import { StageModule } from './stage/stage.module';
 import { RewardModule } from './reward/reward.module';
@@ -13,7 +13,18 @@ import { TopicModule } from './topic/topic.module';
 import { ClientModule } from './client/client.module';
 
 @Module({
-  imports: [FormationModule, TopicModule, ClientModule, EventsModule,PassModule, StageModule, RewardModule, UserModule, PrismaModule, AuthenticationModule],
+  imports: [
+    FormationModule,
+    TopicModule,
+    ClientModule,
+    EventsModule,
+    PassModule,
+    StageModule,
+    RewardModule,
+    UserModule,
+    PrismaModule,
+    AuthenticationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
