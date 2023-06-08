ef2a93b131798589b9056a40f2e22f4ea35ee19d
Sadik Raida <sadik.raida@gmail.com>
2023-06-08 19:12:07 +0200

    recuperation email et role dans le payload

--- back/src/authentication/authentication.controller.ts
+++ back/src/authentication/authentication.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { Public } from './decorators/public.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('authentication')
 export class AuthenticationController {
@@ -12,5 +13,11 @@ export class AuthenticationController {
   @UseGuards(LocalAuthGuard)
   async login(@Request() { body }: any) {
     return this.authService.login(body);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
