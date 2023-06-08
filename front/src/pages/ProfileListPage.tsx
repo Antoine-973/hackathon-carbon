9a031778cdfc123f08845a2cb51615c5e23d148e
alexandrebdry <alexbaudry0@gmail.com>
2023-06-08 11:06:03 +0200

    Fix

--- front/src/pages/ProfileListPage.tsx
+++ front/src/pages/ProfileListPage.tsx
@@ -13,52 +13,50 @@ import {
 
 export default function ProfileListPage() {
     return (
-
-            <Container>
-                <Grid container>
-                    <Grid item xs={12} sm={2}>
-                        <Typography>
-                            Tous les filtres
-                        </Typography>
-                        <Stack>
-                            <FormControl sx={{my:4}}>
-                                <InputLabel variant="standard" htmlFor="client">
-                                    Client
-                                </InputLabel>
-                                <NativeSelect
-                                    defaultValue={'aucun'}
-                                    inputProps={{
-                                        name: 'age',
-                                        id: 'client',
-                                    }}
-                                >
-                                    <option value={'aucun'}>Aucun</option>
-                                    <option value={'pmu'}>PMU</option>
-                                    <option value={'darty'}>Darty</option>
-                                    <option value={'fnac'}>Fnac</option>
-                                </NativeSelect>
-                            </FormControl>
-                            <FormControl sx={{my:4}}>
-                                <InputLabel variant="standard" htmlFor="techno">
-                                    Technologie
-                                </InputLabel>
-                                <NativeSelect
-                                    defaultValue={'aucune'}
-                                    inputProps={{
-                                        name: 'age',
-                                        id: 'techno',
-                                    }}
-                                >
-                                    <option value={'aucune'}>Aucune</option>
-                                    <option value={'react'}>React</option>
-                                    <option value={'vue'}>Vue</option>
-                                    <option value={'angular'}>Angular</option>
-                                </NativeSelect>
-                            </FormControl>
-                        </Stack>
-                    </Grid>
+        <Container>
+            <Grid container>
+                <Grid item xs={12} sm={2}>
+                    <Typography>
+                        Tous les filtres
+                    </Typography>
+                    <Stack>
+                        <FormControl sx={{my:4}}>
+                            <InputLabel variant="standard" htmlFor="client">
+                                Client
+                            </InputLabel>
+                            <NativeSelect
+                                defaultValue={'aucun'}
+                                inputProps={{
+                                    name: 'age',
+                                    id: 'client',
+                                }}
+                            >
+                                <option value={'aucun'}>Aucun</option>
+                                <option value={'pmu'}>PMU</option>
+                                <option value={'darty'}>Darty</option>
+                                <option value={'fnac'}>Fnac</option>
+                            </NativeSelect>
+                        </FormControl>
+                        <FormControl sx={{my:4}}>
+                            <InputLabel variant="standard" htmlFor="techno">
+                                Technologie
+                            </InputLabel>
+                            <NativeSelect
+                                defaultValue={'aucune'}
+                                inputProps={{
+                                    name: 'age',
+                                    id: 'techno',
+                                }}
+                            >
+                                <option value={'aucune'}>Aucune</option>
+                                <option value={'react'}>React</option>
+                                <option value={'vue'}>Vue</option>
+                                <option value={'angular'}>Angular</option>
+                            </NativeSelect>
+                        </FormControl>
+                    </Stack>
                 </Grid>
-            </Container>
-        </Box>
+            </Grid>
+        </Container>
     );
 }
