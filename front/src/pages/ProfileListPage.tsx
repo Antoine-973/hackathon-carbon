1031fd6277a42b523038debb1718f3b1ff658250
alexandrebdry <alexbaudry0@gmail.com>
2023-06-08 11:37:57 +0200

    Search

--- front/src/pages/ProfileListPage.tsx
+++ front/src/pages/ProfileListPage.tsx
@@ -1,62 +1,42 @@
-import {
-    Box,
-    Container,
-    Grid,
-    Typography,
-    Select,
-    MenuItem,
-    FormLabel,
-    FormControl,
-    InputLabel,
-    NativeSelect, Stack
-} from "@mui/material";
+import {Box, Container, FormControl, InputLabel, NativeSelect} from "@mui/material";
+import Selector from "../components/Selector";
+import {SetStateAction, useState} from "react";
+
 
 export default function ProfileListPage() {
+
+    const [client, setClient] = useState();
+    const [techno, setTechno] = useState();
+    const [expertise, setExpertise] = useState();
+
     return (
         <Container>
-            <Grid container>
-                <Grid item xs={12} sm={2}>
-                    <Typography>
-                        Tous les filtres
-                    </Typography>
-                    <Stack>
-                        <FormControl sx={{my:4}}>
-                            <InputLabel variant="standard" htmlFor="client">
-                                Client
-                            </InputLabel>
-                            <NativeSelect
-                                defaultValue={'aucun'}
-                                inputProps={{
-                                    name: 'age',
-                                    id: 'client',
-                                }}
-                            >
-                                <option value={'aucun'}>Aucun</option>
-                                <option value={'pmu'}>PMU</option>
-                                <option value={'darty'}>Darty</option>
-                                <option value={'fnac'}>Fnac</option>
-                            </NativeSelect>
-                        </FormControl>
-                        <FormControl sx={{my:4}}>
-                            <InputLabel variant="standard" htmlFor="techno">
-                                Technologie
-                            </InputLabel>
-                            <NativeSelect
-                                defaultValue={'aucune'}
-                                inputProps={{
-                                    name: 'age',
-                                    id: 'techno',
-                                }}
-                            >
-                                <option value={'aucune'}>Aucune</option>
-                                <option value={'react'}>React</option>
-                                <option value={'vue'}>Vue</option>
-                                <option value={'angular'}>Angular</option>
-                            </NativeSelect>
-                        </FormControl>
-                    </Stack>
-                </Grid>
-            </Grid>
+            <Box sx={{
+                position: 'fixed',
+                top: '100px',
+                display: 'flex',
+                width: '100%',
+            }}>
+                <Selector
+                    title={'Client'}
+                    value={client}
+                    values={['Pmu', 'Darty', 'Fnac']}
+                    handleChange={(event: { target: { value: SetStateAction<undefined>; }; }) => setClient(event.target.value)}
+                 />
+                <Selector
+                    title={'Technologie'}
+                    value={techno}
+                    values={['React', 'Vue', 'Angular']}
+                    handleChange={(event: { target: { value: SetStateAction<undefined>; }; }) => setTechno(event.target.value)}
+                />
+                <Selector
+                    title={'Expertise'}
+                    value={expertise}
+                    values={['Junior', 'Confirmé', 'Expert']}
+                    handleChange={(event: { target: { value: SetStateAction<undefined>; }; }) => setExpertise(event.target.value)}
+                />
+
+            </Box>
         </Container>
     );
 }
