a39942bc17f03e57c6ddd517b692e5846f0c8560
alexandrebdry <alexbaudry0@gmail.com>
2023-06-09 15:38:52 +0200

    Back office formation

--- front/src/services/FormationServices.tsx
+++ front/src/services/FormationServices.tsx
@@ -16,5 +16,34 @@ export const FormationServices = {
 
     },
 
+    createFormation : async (formation) => {
+        try {
+            const response = await fetch(ServicesBases.apiUrl+'/formation', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formation),
+            });
+            return await response.json();
+        }catch(error){
+            return error ;
+        }
+    },
+
+    deleteFormation : async (id) => {
+        try {
+            const response = await fetch(ServicesBases.apiUrl+'/formation/'+id, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            return await response.json();
+        }catch(error){
+            return error ;
+        }
+    }
+
 
 }
